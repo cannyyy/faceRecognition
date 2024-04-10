8a3d0b0c6efb5cde9ff9b0c66cf70432c77796ad
Sevket Can Yorulmaz <can7881@hotmail.com>
2024-04-10 17:04:32 +0300

    faceRecognition
    Recognizing and distinguishing faces taught with Python.

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(r"classifier/haarcascade_frontalface_default.xml")
+
+img = cv2.imread(r"photos/a.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+faces = face_cascade.detectMultiScale(gray, 1.5, 3)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
